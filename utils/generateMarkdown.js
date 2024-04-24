a20f1461ded1e39a413fbaaacc17d8deac229df4
Salokin <nicholas.dpoul@gmail.com>
2024-04-24 09:25:11 -0400

    filled out generate markdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,7 +17,44 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  
+  return `# ${data.title}
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Credits
+
+  ${data.credits}
+
+  ## License
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributions
+
+  ${data.contribution}
+
+  ## Tests
+
+  ${data.test}
+`;
 }
 
 module.exports = generateMarkdown;
